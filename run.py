0880633767a64ff404703480ae976be113ea279b
rocrill <crillyr@tcd.ie>
2021-11-13 19:57:07 +0000

    Add 'select word' function and beginning of 'play game' function. Add hangman ascii pics.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random 
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,6 +13,67 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_words')
 
+HANGMANPICS = [
+    '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+def select_word():
+    """
+    Selects random word from Google Sheet database
+    """
+    words = SHEET.worksheet("words").col_values(1)
+
+    return random.choice(words)
+
+
 def get_user_guess():
     """
     Get user letter guess for word
@@ -21,4 +83,13 @@ def get_user_guess():
     data_str = input("Enter your guess here: ")
     print(f"The data provided is {data_str}")
 
-get_user_guess()
+def play_game():
+    """
+    Plays game steps
+    """
+    answer = select_word() 
+    image = HANGMANPICS[0]
+    print(image)
+    print ('_' * len(answer))
+
+play_game()
